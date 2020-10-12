48849183a2f78bd580d3003b294ea5020517fa5c
hafiizh10 <hafiizh10@gmail.com>
2020-10-12 20:52:27 +0800

    update sidebar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="au theme template">
 
     <!-- Title Page-->
-    <title>Dashboard</title>
+    <title><?= $title ?></title>
 
     <!-- Fontfaces CSS-->
     <link href="<?= base_url('assets/') ?>css/font-face.css" rel="stylesheet" media="all">
